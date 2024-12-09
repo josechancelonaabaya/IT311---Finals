3e1b58ae2d9066ec97b16cfb482135949aadb3af
emarsonayap12 <90960316+emarsonayap12@users.noreply.github.com>
2024-12-09 21:24:49 +0800

    PollutantData.kt  source code

--- AirQualityMonitor/src/main/PollutantData.kt
+++ AirQualityMonitor/src/main/PollutantData.kt
@@ -1,15 +1,6 @@
 
-PollutantData.kt
-/**
- * Data class representing pollutant levels for air quality evaluation.
- *
- * @param co Carbon monoxide level in ppm.
- * @param no2 Nitrogen dioxide level in ppb.
- * @param pm25 Particulate matter (PM2.5) level in µg/m³.
- */
-data class PollutantData(
-    val co: Double,
-    val no2: Double,
-    val pm25: Double
-)
-
+PollutantData.kt 
+// Non-compliant: Poor naming conventions, missing comments, introduced bug
+data class x(val CO: Double, val No2: Double, val pm25: Double) {
+    fun invalidCalculation() = CO + No2 - pm25 * 2 // Irrelevant function with incorrect logic
+}
