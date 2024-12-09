a9f954b830c07fb65c51f9c8fec9ab6fb6eb3356
josechancelonaabaya <2224369@slu.edu.ph>
2024-12-09 23:41:13 +0800

    new update

--- ThresholdAlerts.kt
+++ ThresholdAlerts.kt
@@ -4,6 +4,6 @@ fun CheckThresholds(data: pollutant_data): List<String> {
     if (data.pm25 > 35) alerts.add("High PM2.5!") 
     if (data.No2 > 75) { alerts.add("High NO2!") } 
     if (data.CO > 20) { alerts.add("High CO!") }
-    if (data.CO < 0 || data.No2 < 0 || data.pm25 < 0) alerts.add("Negative values are not allowed") // Unhandled scenario
+    if (data.CO < 0 || data.No2 < 0 || data.pm25 < 0) alerts.add("Negative values are not allowed") 
     return alerts
 }
