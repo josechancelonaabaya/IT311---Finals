9799a4edc755ab079cd4c56e15f6d448d1b64207
josechancelonaabaya <2224369@slu.edu.ph>
2024-12-09 21:53:48 +0800

    deleted all files in main

--- AirQualityMonitor/src/main/PollutantData.kt
+++
@@ -1,6 +0,0 @@
-
-PollutantData.kt 
-// Non-compliant: Poor naming conventions, missing comments, introduced bug
-data class x(val CO: Double, val No2: Double, val pm25: Double) {
-    fun invalidCalculation() = CO + No2 - pm25 * 2 // Irrelevant function with incorrect logic
-}
